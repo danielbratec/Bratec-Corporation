0f87a7c7e6fbb10b5eeb5787842f603ecc18c83f
danielbratec <danielmoraissoares317@gmail.com>
2025-02-25 19:26:44 -0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,5 @@
+const elemento = document.querySelector(".minhaClasse");
+elemento.innerHTML = "<b>Bratec</b>";
+const elemento = document.querySelector(".minhaClasse");
+elemento.innerHTML = "<b>PetroBras</b>";
+const elementos = document.querySelectorAll
